b50ac3425eb0c82f8b778bf0a06a2f92f2f34871
Srivandhi <srivandhi264@gmail.com>
2024-10-27 13:29:56 +0530

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 package Student_Management_system;
-
-public class Main {
-
+import java.util.*;
+import java.io.*;
+class Main
+{
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in);
+        int n,regno;
+        char grade;
+        String dept,name;
+        System.out.print("Enter number of students = ");
+         n=reader.nextInt();
+        File f=new File("Student.txt");
+    }
 }
